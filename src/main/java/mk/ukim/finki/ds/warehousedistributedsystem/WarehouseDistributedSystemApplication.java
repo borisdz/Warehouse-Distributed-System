7a48ae8083e6569dc86cba1cd2b9ae7713fe61b8
Boris Djotov <borisdzotov@gmail.com>
2025-12-18 10:39:55 +0100

    initialize WarehouseDistributedSystem project with Maven and Spring Boot

---
+++ src/main/java/mk/ukim/finki/ds/warehousedistributedsystem/WarehouseDistributedSystemApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ds.warehousedistributedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseDistributedSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseDistributedSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/ds/warehousedistributedsystem/WarehouseDistributedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ds.warehousedistributedsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseDistributedSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
